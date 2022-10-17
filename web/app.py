8af4679341fc5725c2f717957feb9ac7768ad98e
samuel <samuel86102@gmail.com>
2022-10-17 11:13:31 +0800

    update

--- web/app.py
+++ web/app.py
@@ -1,12 +1,29 @@
 from flask import Flask, Response
-from flask import render_template
+from flask import render_template, redirect
+
+
+from pythonosc.udp_client import SimpleUDPClient
+
+ip = "127.0.0.1"
+port = 1337
+
+client = SimpleUDPClient(ip, port)  # Create client
+
+#client.send_message("/some/address", [1, 2., "hello"])  # Send message with int, float and string
+
 
 app = Flask(__name__)
+
+
 
 @app.route('/player.html')
 def player():
     return render_template('player.html') 
 
+@app.route('/test')
+def test():
+    client.send_message("/something/address", 123)   # Send float message
+    return redirect('/')
 
 @app.route('/')
 def index():
