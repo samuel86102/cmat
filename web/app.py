1d2ca6efada7c58d4760187fd57e7f2bd44cae0e
samuel <samuel86102@gmail.com>
2022-11-20 17:29:21 +0800

    update

--- web/app.py
+++ web/app.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, request
 from flask import render_template, redirect
 from pythonosc.udp_client import SimpleUDPClient
 
+player_toggle = 0
 
 def send_osc_msg():
 
@@ -17,16 +18,41 @@ def send_osc_msg_a():
     client = SimpleUDPClient(ip, port)  # Create client
     client.send_message("/toggle_roll", 0)   # Send float message
 
+def osc_play():
+    ip = "127.0.0.1"
+    port = 7400 
+    client = SimpleUDPClient(ip, port)  # Create client
+    client.send_message("/msg", 1)   # Send float message
+
+def osc_pause():
+    ip = "127.0.0.1"
+    port = 7400 
+    client = SimpleUDPClient(ip, port)  # Create client
+    client.send_message("/msg", 0)   # Send float message
+
+def osc_value(slide_val):
+    ip = "127.0.0.1"
+    port = 7400 
+    client = SimpleUDPClient(ip, port)  # Create client
+    client.send_message("/vol", int(slide_val))   # Send float message
+
 
 app = Flask(__name__)
 
 
+@app.route("/test", methods=["POST"])
+def test():
+    slide_val = request.form["name_of_slider"]
+    print(slide_val)
+    osc_value(slide_val)
+    #return slide_val
+    return redirect('./player')
+
 @app.route('/player')
 def player():
-
     #page = request.args.get('page', default = 1, type = int)
     set_name = request.args.get('set', default = '*', type = str)
-    print(set_name)
+    #print(set_name)
 
     return render_template('player.html', set_name = set_name) 
 
@@ -38,6 +64,18 @@ def player():
 def player():
     return render_template('player.html') 
 '''
+
+
+@app.route('/play')
+def play():
+    osc_play()
+    return redirect('./player')
+
+@app.route('/pause')
+def pause():
+    osc_pause()
+    return redirect('./player')
+
 @app.route('/send_osc')
 def send_osc():
     send_osc_msg()
